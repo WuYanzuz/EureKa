782b69fcdb36985bbff574f954d9ba1cf4a5da93
yanzu <1245673955@qq.com>
2021-11-30 12:23:31 +0800

    Eureka配置

--- Eureka_consumer/src/main/java/edu/jxut/controller/GoodController.java
+++ Eureka_consumer/src/main/java/edu/jxut/controller/GoodController.java
@@ -3,15 +3,23 @@ package edu.jxut.controller;
 import edu.jxut.domain.Good;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.List;
+
 @RestController
 @RequestMapping("/order")
 public class GoodController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Value("${rest.url}")
     private String url;
@@ -21,8 +29,19 @@ public class GoodController {
 
     @GetMapping("/goods/{id}")
     public Good findGoodById(@PathVariable("id") String id){
-        System.out.println(url);
-        Good good = restTemplate.getForObject(url, Good.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("EUREKA_PROVIDER");
+        ServiceInstance serviceInstance = instances.get(0);
+        String host = serviceInstance.getHost();
+        String port = String.valueOf(serviceInstance.getPort());
+        URI uri = serviceInstance.getUri();
+        String s = uri.toString();
+        System.out.println(s);
+
+        System.out.println(host);
+        System.out.println(port);
+        String lj="http://"+host+":"+port+"/goods/getGoods/id=1";
+
+        Good good = restTemplate.getForObject(lj, Good.class);
         return good;
     }
 }
